2021ee7f26be0262fc3d22c92970f2d98dae160c
waqasshah191 <syedwaqasshah191@gmail.com>
2021-06-13 16:01:28 -0600

    creditcard table updated

--- client/core/Table.js
+++ client/core/Table.js
@@ -2,8 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
 import TabackgroundImg from './../assets/images/moneyback.jpg';
-import td1 from './../assets/images/td1.jpg';
+//import tdc1 from './../assets/images/allInclusiveChequing-Account.svg';
+import tdc1 from './../assets/images/TDAeroplanVisaInfiniteCard.jpg';
+import tdcc2 from './../assets/images/tdcc2.jpg';
+import tdcc3 from './../assets/images/tdcc3.jpg';
+import tdcc4 from './../assets/images/tdcc4.jpg';
+import tdc5 from './../assets/images/studentAccount.svg';
 
+import Image from "material-ui-image";
 
 const useStyles = makeStyles({
   root: {
@@ -14,12 +20,12 @@ const useStyles = makeStyles({
     boxSizing: 'border-box'
   },
   title: {
-    fontSize: 18,
+    fontSize: 30,
     fontWeight: 'bold',
     textAlign: 'center',
   },
   detail: {
-    fontSize: 30,
+    fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
   },
@@ -27,12 +33,14 @@ const useStyles = makeStyles({
     textAlign: 'center',
   },
   table: { textAlign: 'left' },
-  media: { maxWidth: 60 }
+  media: {
+        objectFit: 'contain',
+ }
 });
 
 export default function BasicTable() {
 
-  const [rows, setRows] = useState([]);
+  //const [rows, setRows] = useState([]);
 
 
   // useEffect(() => {
@@ -52,7 +60,7 @@ export default function BasicTable() {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell className={classes.title}>Credit cards</TableCell>
+            <TableCell className={classes.title}>Credit Cards</TableCell>
             <TableCell className={classes.title}>Annual Fee</TableCell>
             <TableCell className={classes.title}>Interest: Purchases</TableCell>
             <TableCell className={classes.title}>Interest: Cash Advances</TableCell>
@@ -62,11 +70,37 @@ export default function BasicTable() {
         </TableHead>
         <TableBody>
             <TableRow>
-              <TableCell className={classes.detail}><img src={td1}/>TD All-Inclusive Banking Plan</TableCell>
-              <TableCell className={classes.detail}>$120</TableCell>
-              <TableCell className={classes.detail}>20.99%</TableCell>
+              {/* <TableCell className={classes.detail}><img src={tdc1}/>TD All-Inclusive Banking Plan</TableCell> */}
+              <TableCell className={classes.detail}><Image src={tdc1}/>TD® Aeroplan® Visa Infinite* Card</TableCell>
+              <TableCell className={classes.detail}>$139</TableCell>
+              <TableCell className={classes.detail}>19.99%</TableCell>
               <TableCell className={classes.detail}>22.99%</TableCell>
-              <TableCell className={classes.detail}>$50</TableCell>
+              <TableCell className={classes.detail}>$75</TableCell>
+              <TableCell className={classes.detail}>Earn up to 20,000 Aeroplan points </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className={classes.detail}><Image src={tdcc2}/>TD Cash Back Visa* Card</TableCell>
+              <TableCell className={classes.detail}>$0</TableCell>
+              <TableCell className={classes.detail}>19.99%</TableCell>
+              <TableCell className={classes.detail}>22.99%</TableCell>
+              <TableCell className={classes.detail}>$0</TableCell>
+              <TableCell className={classes.detail}>Earn 1% in Cash Back Dollars on eligible grocery Purchases and gas Purchases</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className={classes.detail}><Image src={tdcc3}/>TD Business Travel Visa* Card</TableCell>
+              <TableCell className={classes.detail}>$149</TableCell>
+              <TableCell className={classes.detail}>19.99%</TableCell>
+              <TableCell className={classes.detail}>22.99%</TableCell>
+              <TableCell className={classes.detail}>49</TableCell>
+              <TableCell className={classes.detail}>Welcome Bonus1: Apply today and get 30,000 TD Rewards Points upon first purchase.</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className={classes.detail}><Image src={tdcc4}/>TD Rewards Visa Card</TableCell>
+              <TableCell className={classes.detail}>$0</TableCell>
+              <TableCell className={classes.detail}>19.99%</TableCell>
+              <TableCell className={classes.detail}>22.99%</TableCell>
+              <TableCell className={classes.detail}>0</TableCell>
+              <TableCell className={classes.detail}>Earn 2 TD Rewards Points for every $1 in grocery, restaurant and fast food purchases</TableCell>
             </TableRow>
         </TableBody>
       </Table>
